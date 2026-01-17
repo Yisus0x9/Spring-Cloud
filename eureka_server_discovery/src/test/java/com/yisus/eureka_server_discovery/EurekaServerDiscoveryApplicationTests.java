c96f332143546c63ab10edb4241d561da985af35
Yisus0x19 <jesuspenarrieta03@gmail.com>
2026-01-17 13:18:11 -0600

    creación de un eureka server

---
+++ eureka_server_discovery/src/test/java/com/yisus/eureka_server_discovery/EurekaServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yisus.eureka_server_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
